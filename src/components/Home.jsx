2466e92014b9684244b025d6a5fd4aba59146986
Bishnu Kumar Yadav <bishnuyadav857@gmail.com>
2025-06-23 21:27:44 +0545

    Fix import case issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
-import Card from './card';
+import Card from './Card';
 import Form from "./Form";
 import Footer from './Footer';
 import AboutContent from './AboutContent';
